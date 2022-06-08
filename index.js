5c609cd9c457701ccf5fda43b661d2ca89f80d74
Ngaicode <mjombakevin376@gmail.com>
2022-06-08 22:53:14 +0300

    Done.

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 function receivesAFunction(callback) {
   callback();
 }
+receivesAFunction();
 
 function returnsANamedFunction() {
   return function namedFn() {
     console.log("this is a named function");
   };
 }
-
+returnsANamedFunction();
 function returnsAnAnonymousFunction() {
   return () => console.log("this is an anonymous function");
 }
+returnsAnAnonymousFunction();
